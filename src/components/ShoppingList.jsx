6d6a5dde5068e2c8858e9b45d5b2769934945a78
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-05-05 21:50:53 +1200

    Update the styling for the ShoppingList divs to be centered

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,20 +1,22 @@
 function ShoppingList() {
   return (
-    <div className="bg-white shadow-lg rounded-lg px-4 py-6">
-      <h3 className="font-mono font-bold text-xl text-center mb-4">
-        Shopping List
-      </h3>
-      <ul className="divide-y divide-gray-300">
-        <li className="flex items-center py-2">
-          <p className="font-mono text-gray-900 text-lg">Item 1</p>
-        </li>
-        <li className="flex items-center py-2">
-          <p className="font-mono text-gray-900 text-lg">Item 2</p>
-        </li>
-        <li className="flex items-center py-2">
-          <p className="font-mono text-gray-900 text-lg">Item 3</p>
-        </li>
-      </ul>
+    <div className="max-w-full justify-self-center w-full">
+      <div className="bg-gray-300 shadow-lg rounded-lg px-4 py-6">
+        <h3 className="font-mono font-bold text-xl text-center mb-4">
+          Shopping List
+        </h3>
+        <ul className="divide-y divide-gray-300">
+          <li className="flex items-center py-2">
+            <p className="font-mono text-gray-900 text-lg">Item 1</p>
+          </li>
+          <li className="flex items-center py-2">
+            <p className="font-mono text-gray-900 text-lg">Item 2</p>
+          </li>
+          <li className="flex items-center py-2">
+            <p className="font-mono text-gray-900 text-lg">Item 3</p>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
