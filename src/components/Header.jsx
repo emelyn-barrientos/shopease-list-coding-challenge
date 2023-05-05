4b024dcd297c24221598facf11524bbcaf917354
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-05-05 20:19:38 +1200

    Update fonts for heading tags

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 function Header() {
   return (
     <>
-      <h1 className="text-3xl text-center font-bold underline">ShopEase</h1>
-      <h2 className="text-2xl text-center">
+      <h1 className="font-sans text-3xl text-center font-bold underline">
+        ShopEase
+      </h1>
+      <h2 className="font-sans text-2xl text-center">
         Easily Create & Manage Your Shopping Lists!
       </h2>
     </>
