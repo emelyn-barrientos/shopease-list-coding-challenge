e6437be4aa56f9f3b488275f79b2b59e15b86a13
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-05-06 09:29:42 +1200

    Add required attribute to form

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -21,6 +21,7 @@ function AddItemForm(props) {
           value={newItem}
           onChange={handleNewItemChange}
           className="w-full font-mono rounded-md border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400 mr-4"
+          required
         />
         <button
           type="submit"
