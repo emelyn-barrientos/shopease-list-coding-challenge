7b8e961604eddcf2df9ec109853601da8ca5b4a0
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-05-05 21:23:16 +1200

    Center grid

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <Header />
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-3 gap-4 justify-center">
         <ShoppingList />
         <ShoppingList />
         <ShoppingList />
