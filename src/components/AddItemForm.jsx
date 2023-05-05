baf0e5b0082dda31ba81c0685881cb3e03e6436c
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-05-05 22:53:21 +1200

    Add margin right to input class

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -20,7 +20,7 @@ function AddItemForm(props) {
           type="text"
           value={newItem}
           onChange={handleNewItemChange}
-          className="w-full rounded-md border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+          className="w-full rounded-md border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400 mr-4"
         />
         <button
           type="submit"
