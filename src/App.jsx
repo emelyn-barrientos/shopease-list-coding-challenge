1118818646ebdeaa68392954a7c29b6af381d882
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-05-05 22:27:09 +1200

    Remove useState for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import Header from './components/Header'
 import ShoppingList from './components/ShoppingList'
-import { useState } from 'react'
 
 function App() {
-  const [items, setItems] = useState([])
   return (
     <>
       <Header />
       <div className="grid grid-cols-3 gap-10 mt-16 mx-60">
-        <ShoppingList items={items} />
-        <ShoppingList items={items} />
-        <ShoppingList items={items} />
+        <ShoppingList />
+        <ShoppingList />
+        <ShoppingList />
       </div>
     </>
   )
