0ebb29a4159d8c7bfe616029920ce1461458175a
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-05-05 19:54:36 +1200

    Create components folder and new Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <>
+      <h1 className="text-3xl text-center font-bold underline">ShopEase</h1>
+      <h2 className="text-2xl text-center">
+        Easily Create & Manage Your Shopping Lists!
+      </h2>
+    </>
+  )
+}
+
+export default Header
