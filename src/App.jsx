6b5cf2d38970016d2273bc7cc774ce6078525716
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-05-05 20:32:16 +1200

    Import ShoppingList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import Header from './components/Header'
+import ShoppingList from './components/ShoppingList'
 
 function App() {
-  return <Header />
+  return (
+    <>
+      <Header />
+      <ShoppingList />
+      <ShoppingList />
+      <ShoppingList />
+    </>
+  )
 }
 
 export default App
