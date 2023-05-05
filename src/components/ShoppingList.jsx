d1fe94f25e16e847d01dbd2f969eff78cc0689f2
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-05-05 20:31:57 +1200

    Create new ShoppingList component

---
+++ src/components/ShoppingList.jsx
@@ -0,0 +1,14 @@
+function ShoppingList() {
+  return (
+    <>
+      <h2>Shopping List</h2>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </>
+  )
+}
+
+export default ShoppingList
