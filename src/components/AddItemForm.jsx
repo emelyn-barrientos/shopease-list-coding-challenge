52a2498f0e4de2657b7affa7bc7ffb4874066279
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-05-05 22:50:26 +1200

    Add flex styling to form and new div for form around form items

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -14,19 +14,21 @@ function AddItemForm(props) {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={newItem}
-        onChange={handleNewItemChange}
-        className="w-full rounded-md border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-      />
-      <button
-        type="submit"
-        className="bg-white hover:bg-blue-600 text-gray-900 hover:text-white font-mono text-md rounded-md px-4 py-2"
-      >
-        GO
-      </button>
+    <form onSubmit={handleSubmit} className="flex justify-between">
+      <div className="flex">
+        <input
+          type="text"
+          value={newItem}
+          onChange={handleNewItemChange}
+          className="w-full rounded-md border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        />
+        <button
+          type="submit"
+          className="bg-white hover:bg-blue-600 text-gray-900 hover:text-white font-mono text-md rounded-md px-4 py-2"
+        >
+          GO
+        </button>
+      </div>
     </form>
   )
 }
