25af9d85cfe702e650d7525d555333a7000a163c
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-05-05 20:45:48 +1200

    Add initial div styling shopping lists

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,8 +1,10 @@
 function ShoppingList() {
   return (
-    <div>
-      <h3 className="font-mono font-bold text-xl text-center">Shopping List</h3>
-      <ul>
+    <div className="bg-white shadow-lg rounded-lg px-4 py-6">
+      <h3 className="font-mono font-bold text-xl text-center mb-4">
+        Shopping List
+      </h3>
+      <ul className="divide-y divide-gray-300">
         <li className="text-center">Item 1</li>
         <li className="text-center">Item 2</li>
         <li className="text-center">Item 3</li>
