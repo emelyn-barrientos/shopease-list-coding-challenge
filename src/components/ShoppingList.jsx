9d475aa98742db3de0eb9bd676822e9cf5079d64
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-05-06 18:25:23 +1200

    Fix form to bottom of ShoppingList gray div

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -13,8 +13,8 @@ function ShoppingList({ listName, items }) {
       <h3 className="font-mono font-bold text-xl text-center mb-4">
         {listName}
       </h3>
-      <div className="bg-gray-300 shadow-lg rounded-lg px-4 py-6 h-full">
-        <ul className="divide-y divide-gray-300">
+      <div className="bg-gray-300 shadow-lg rounded-lg px-4 py-6 h-full flex flex-col justify-between">
+        <ul className="divide-y divide-gray-300 flex-grow">
           {itemsState.map((item, index) => (
             <li
               key={index}
@@ -24,7 +24,9 @@ function ShoppingList({ listName, items }) {
             </li>
           ))}
         </ul>
-        <AddItemForm onAddItem={handleAddItem} />
+        <div className="self-end">
+          <AddItemForm onAddItem={handleAddItem} className="mt-4" />
+        </div>
       </div>
     </div>
   )
