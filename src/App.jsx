82a6d40b5c5b3b14c06fad5975307e9fa3426d0b
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-05-06 17:16:12 +1200

    Add items prop for each ShoppingList

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,15 @@ function App() {
     <>
       <Header />
       <div className="grid grid-cols-3 gap-10 mt-16 mx-60">
-        <ShoppingList listName={'List 1'} />
-        <ShoppingList listName={'List 2'} />
-        <ShoppingList listName={'List 3'} />
+        <ShoppingList
+          listName={'List 1'}
+          items={['Item 1', 'Item 2', 'Item 3']}
+        />
+        <ShoppingList
+          listName={'List 2'}
+          items={['Item 4', 'Item 5', 'Item 3']}
+        />
+        <ShoppingList listName={'List 3'} items={['Item 6']} />
       </div>
     </>
   )
