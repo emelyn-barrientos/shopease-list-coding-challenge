f387aedca91cd5ca9956d15f7c9ee45ba7b599a3
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-05-06 22:21:45 +1200

    Create a onMoveItem handler func

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,46 @@ function App() {
     list3: ['Item 6'],
   })
 
+  const handleMoveItem = (item, fromList, toList) => {
+    const fromListItems = [...lists[fromList]]
+    const toListItems = [...lists[toList]]
+    const index = fromListItems.indexOf(item)
+
+    if (index > -1) {
+      if (index > -1) {
+        fromListItems.splice(index, 1)
+        toListItems.push(item)
+        setLists({
+          ...lists,
+          [fromList]: fromListItems,
+          [toList]: toListItems,
+        })
+      }
+    }
+  }
+
   return (
     <>
       <Header />
       <div className="grid grid-cols-3 gap-10 mt-16 mx-60">
-        <ShoppingList listName={'List 1'} items={lists.list1} />
-        <ShoppingList listName={'List 2'} items={lists.list2} />
-        <ShoppingList listName={'List 3'} items={lists.list3} />
+        <ShoppingList
+          listName={'List 1'}
+          items={lists.list1}
+          onMoveItem={handleMoveItem}
+          fromList="list1"
+        />
+        <ShoppingList
+          listName={'List 2'}
+          items={lists.list2}
+          onMoveItem={handleMoveItem}
+          fromList="list2"
+        />
+        <ShoppingList
+          listName={'List 3'}
+          items={lists.list3}
+          onMoveItem={handleMoveItem}
+          fromList="list3"
+        />
       </div>
     </>
   )
