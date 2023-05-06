46287cf97afbdfa8d949e3cac543d997247ea05e
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-05-06 23:25:09 +1200

    Update items as array initial state & update handleAddItem func

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -2,10 +2,10 @@ import AddItemForm from './AddItemForm'
 import { useState } from 'react'
 
 function ShoppingList({ listName, items, onMoveItem, fromList }) {
-  const [itemsState, setItemsState] = useState(items)
+  const [itemsState, setItemsState] = useState([...items])
 
   const handleAddItem = (newItem) => {
-    setItemsState([...itemsState, newItem])
+    setItemsState((prevState) => [...prevState, newItem])
   }
 
   const handleMoveItem = (index, selectedList) => {
