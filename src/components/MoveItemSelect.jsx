57f25cc1bdd88bcdf8fbcc65b3a506076c710a12
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-05-07 13:01:04 +1200

    Add initial classs styling for select

--- src/components/MoveItemSelect.jsx
+++ src/components/MoveItemSelect.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function MoveItemSelect({ options, onChange }) {
   return (
-    <select onChange={onChange}>
+    <select
+      onChange={onChange}
+      className="block w-full bg-white border border-gray-300 py-2 px-3 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+    >
       <option>Select list</option>
       {options.map((option) => (
         <option key={option.value} value={option.value}>
