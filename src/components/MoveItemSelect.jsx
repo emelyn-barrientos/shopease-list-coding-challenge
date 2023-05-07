aee3a167816f2939ba9f2eb76658ef7c78fa5236
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-05-07 13:02:10 +1200

    Change Option text

--- src/components/MoveItemSelect.jsx
+++ src/components/MoveItemSelect.jsx
@@ -6,7 +6,7 @@ function MoveItemSelect({ options, onChange }) {
       onChange={onChange}
       className="block w-full bg-white border border-gray-300 py-2 px-3 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
     >
-      <option>Select list</option>
+      <option>Move Item</option>
       {options.map((option) => (
         <option key={option.value} value={option.value}>
           {option.label}
